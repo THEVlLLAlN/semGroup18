790385aefa3c8d8a49d3f45e9e98ddf69c67cc61
Joseph Marshall-Brydson <40323478@live.napier.ac.uk>
2019-03-07 14:59:33 +0000

    integration test changes

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,72 @@
+package com.napier.sem;
+import java.sql.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("db");
+    }
+
+    @Test
+    void countryTest() {
+
+        country countryA = new country();
+
+        countryA.setCode("XD");
+        assertEquals(countryA.getCode(), "XD");
+
+        countryA.setName("Jon Callahan");
+        assertEquals(countryA.getName(), "Jon Callahan");
+
+        countryA.setContinent("Irozan");
+        assertEquals(countryA.getContinent(), "Irozan");
+
+        countryA.setRegion("asujas");
+        assertEquals(countryA.getRegion(), "asujas");
+
+        countryA.setIndepYear(2012);
+        assertEquals(countryA.getIndepYear(), 2012);
+
+        countryA.setSurfaceArea(12345);
+        assertEquals(countryA.getSurfaceArea(), 12345);
+
+        countryA.setPopulation(450000);
+        assertEquals(countryA.getPopulation(), 450000);
+
+        countryA.setLifeExpectancy(12345);
+        assertEquals(countryA.getLifeExpectancy(), 12345);
+
+        countryA.setGNP(12345);
+        assertEquals(countryA.getGNP(), 12345);
+
+        countryA.setGNPOld(12345);
+        assertEquals(countryA.getGNPOld(), 12345);
+
+        countryA.setLocalName("ASDf");
+        assertEquals(countryA.getLocalName(), "ASDf");
+
+        countryA.setGovernmentForm("Democracy");
+        assertEquals(countryA.getGovernmentForm(), "Democracy");
+
+        countryA.setHeadOfState("Dr Eggman");
+        assertEquals(countryA.getHeadOfState(), "Dr Eggman");
+
+        countryA.setCapital(123);
+        assertEquals(countryA.getCapital(), 123);
+
+        countryA.setCode2("Uwu");
+        assertEquals(countryA.getCode2(), "Uwu");
+
+    }
+}
