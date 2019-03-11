7bec25bb20f3c96fd33451e2a5ca130a551e54b7
Ben Sutton <BenThomasSutton@gmail.com>
2019-03-11 04:18:57 +0000

    Basic Integration Tests + Get City function

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class AppIntegrationTest
